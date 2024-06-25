4ce735516901c31cf0c5637b9f9679e8f5d92ba9
bradenmweight <braden.m.weight@gmail.com>
2024-06-25 16:11:31 -0400

    Added frequency scan to comparison

--- pQED_scQED_Comparison/main.py
+++ pQED_scQED_Comparison/main.py
@@ -25,14 +25,8 @@ def get_system():
 
     return mol, mf
 
-if ( __name__ == "__main__"):
 
-    # DEFINE THE SYSTEM
-    mol, mf = get_system()
-    NSTATES  = 10
-    cavity_freq    = np.array([11.5/27.2114])
-    cavity_epol    = np.array([1, 1, 1])
-    LAM_LIST = np.arange( 0.0, 0.21, 0.01 )
+def lambda_scan( mol, mf, NSTATES, cavity_freq, cavity_epol, LAM_LIST ):
 
     # GET THE DATA
     E_scQED   = np.zeros( (len(LAM_LIST), NSTATES) )
@@ -65,7 +59,7 @@ if ( __name__ == "__main__"):
     plt.xlim(LAM_LIST[0],LAM_LIST[-1])
     plt.ylim(9,15)
     plt.tight_layout()
-    plt.savefig("pQED_scQED.jpg", dpi=300)
+    plt.savefig("pQED_scQED_lambda.jpg", dpi=300)
     plt.clf()
 
     # PLOT THE DIFFERENCE
@@ -78,18 +72,95 @@ if ( __name__ == "__main__"):
     plt.legend()
     plt.xlim(LAM_LIST[0],LAM_LIST[-1])
     plt.tight_layout()
-    plt.savefig("pQED_scQED_DIFF.jpg", dpi=300)
+    plt.savefig("pQED_scQED_DIFF_lambda.jpg", dpi=300)
     plt.clf()
 
     # PLOT THE PHOTON NUMBER
     color_list = ["black", "red", "blue", "green", "purple", "orange", "cyan", "magenta", "brown", "pink", "gray"]
     for state in range( NSTATES ):
         plt.plot( LAM_LIST, PHOT_pQED[:,state], "-", c=color_list[state], lw=5, label=f"State {state+1}" )
-        #plt.plot( LAM_LIST, PHOT_scQED[:,state], "o", c=color_list[state], lw=5, label=f"State {state+1}" )
     plt.xlabel( "Coupling Strength, $\lambda_\mathrm{c}$ (a.u.)" , fontsize=15 )
     plt.ylabel( "Photon Number, $\langle \hat{a}^\dag \hat{a} \\rangle$", fontsize=15 )
     plt.legend()
     plt.xlim(LAM_LIST[0],LAM_LIST[-1])
     plt.tight_layout()
-    plt.savefig("pQED_scQED_PHOT.jpg", dpi=300)
+    plt.savefig("pQED_scQED_PHOT_lambda.jpg", dpi=300)
     plt.clf()
+
+def freq_scan( mol, mf, NSTATES, cavity_freq_list, cavity_epol, cavity_lambda ):
+
+    # GET THE DATA
+    E_scQED   = np.zeros( (len(cavity_freq_list), NSTATES) )
+    E_pQED    = np.zeros( (len(cavity_freq_list), NSTATES+1) )
+    PHOT_pQED = np.zeros( (len(cavity_freq_list), NSTATES+1) )
+    for wci,cavity_freq in enumerate(cavity_freq_list):
+        print( "Working on wc = %1.3f eV" % (cavity_freq*27.2114) )
+        E_scQED[wci,:] = scQED.main( mol, mf, cavity_lambda, [cavity_freq], cavity_epol, NSTATES )
+        E_pQED[wci,:], PHOT_pQED[wci,:]  = pQED.main(WFNS=False, NSTATES=NSTATES+1, NEL=31, NF=10, EPOL=cavity_epol, 
+                                    LAMBDA=cavity_lambda, WC=cavity_freq, HAM="JC", SUBSPACE="TRUNCATED", doPHOT=True)
+    
+    # CONVERT TO eV
+    E_scQED *= 27.2114
+    E_pQED  *= 27.2114
+    cavity_freq_list *= 27.2114
+
+    # PLOT THE DATA
+    for state in range( NSTATES ):
+        if ( state == 0 ):
+            plt.plot( cavity_freq_list, E_pQED[:,state+1] - E_pQED[:,0], "-", lw=5, c='black', label="pQED" )
+            plt.plot( cavity_freq_list, E_scQED[:,state], "o", lw=3, c='red', label="scQED" )
+        else:
+            plt.plot( cavity_freq_list, E_pQED[:,state+1] - E_pQED[:,0], "-", lw=5, c='black' )
+            plt.plot( cavity_freq_list, E_scQED[:,state], "o", lw=3, c='red' )
+    plt.xlabel( "Cavity Frequency, $\omega_\mathrm{c}$ (eV)" , fontsize=15 )
+    plt.ylabel( "Transition Energy (eV)", fontsize=15 )
+    plt.legend()
+    plt.xlim(cavity_freq_list[0],cavity_freq_list[-1])
+    plt.ylim(9,15)
+    plt.tight_layout()
+    plt.savefig("pQED_scQED_freq.jpg", dpi=300)
+    plt.clf()
+
+    # PLOT THE DIFFERENCE
+    for state in range( NSTATES ):
+        dE_pQED = E_pQED[:,state+1] - E_pQED[:,0]
+        dE      = dE_pQED - E_scQED[:,state]
+        plt.plot( cavity_freq_list, dE, "-", lw=5, label=f"State {state+1}" )
+    plt.xlabel( "Cavity Frequency, $\omega_\mathrm{c}$ (eV)" , fontsize=15 )
+    plt.ylabel( "Energy Difference, $E^\mathrm{pQED} - E^\mathrm{scQED}$ (eV)", fontsize=15 )
+    plt.legend()
+    plt.xlim(cavity_freq_list[0],cavity_freq_list[-1])
+    plt.tight_layout()
+    plt.savefig("pQED_scQED_DIFF_freq.jpg", dpi=300)
+    plt.clf()
+
+    # PLOT THE PHOTON NUMBER
+    color_list = ["black", "red", "blue", "green", "purple", "orange", "cyan", "magenta", "brown", "pink", "gray"]
+    for state in range( NSTATES ):
+        plt.plot( cavity_freq_list, PHOT_pQED[:,state], "-", c=color_list[state], lw=5, label=f"State {state+1}" )
+    plt.xlabel( "Cavity Frequency, $\omega_\mathrm{c}$ (eV)" , fontsize=15 )
+    plt.ylabel( "Photon Number, $\langle \hat{a}^\dag \hat{a} \\rangle$", fontsize=15 )
+    plt.legend()
+    plt.xlim(cavity_freq_list[0],cavity_freq_list[-1])
+    plt.tight_layout()
+    plt.savefig("pQED_scQED_PHOT_freq.jpg", dpi=300)
+    plt.clf()
+
+if ( __name__ == "__main__"):
+
+    # DEFINE THE SYSTEM
+    mol, mf = get_system()
+    NSTATES  = 10
+    cavity_freq    = np.array([11.5/27.2114])
+    cavity_epol    = np.array([1, 1, 1])
+    LAM_LIST = np.arange( 0.0, 0.21, 0.01 )
+    #lambda_scan( mol, mf, NSTATES, cavity_freq, cavity_epol, LAM_LIST )
+
+
+    # DEFINE THE SYSTEM
+    mol, mf = get_system()
+    NSTATES  = 10
+    cavity_freq_list    = np.arange(9.0,15.05,0.25)/27.2114
+    cavity_epol    = np.array([1, 1, 1])
+    LAMDA = 0.05
+    freq_scan( mol, mf, NSTATES, cavity_freq_list, cavity_epol, LAMDA )
